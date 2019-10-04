ebecc1e170bfd6f036a113bd1a43dedd43c86e15
Eddy Brown <eddybrown@me.com>
2019-10-04 16:50:06 +0100

    More tests
    Added some of my own tests so that questions that are to be published in the future are not shown, and questions published in the past are.

--- polls/tests.py
+++ polls/tests.py
@@ -103,3 +103,22 @@ class QuestionDetailViewTests(TestCase):
         url = reverse('polls:detail', args=(past_question.id,))
         response = self.client.get(url)
         self.assertContains(response, past_question.question_text)
+
+class QuestionResultViewTests(TestCase):
+	def test_future_question(self):
+		"""
+		The result view will not display a question with pub_date in the future.
+		"""
+		future_question = create_question(question_text='Future question.', days=5)
+		url = reverse('polls:results', args=(future_question.id,))
+		response = self.client.get(url)
+		self.assertEqual(response.status_code, 404)
+
+	def test_past_question(self):
+		"""
+		The result view will display a question with pub_date in the past.
+		"""
+		past_question = create_question(question_text='Past question.', days=-5)
+		url = reverse('polls:results', args=(past_question.id,))
+		response = self.client.get(url)
+		self.assertContains(response, past_question.question_text)
